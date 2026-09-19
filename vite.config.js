e34fde3824e9b052cf62ad83c11c69a3be161ca8
lazar-jankovic <lj20210228@student.fon.bg.ac.rs>
2026-09-19 02:47:38 +0200

    izmena

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+  css: {
+    transformer: 'postcss', // Prilagođava obradu PostCSS-u
+  },
+  build: {
+    cssMinify: 'esbuild', // Koristi esbuild umesto lightningcss
+  },
   plugins: [
     react(),
     VitePWA({
@@ -20,16 +26,16 @@ export default defineConfig({
           {
             src: 'pwa-192x192.png',
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/png',
           },
           {
             src: 'pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'any maskable'
-          }
-        ]
-      }
-    })
-  ]
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
 });
